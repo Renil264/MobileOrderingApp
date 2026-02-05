f6f8bfca015641ec1384e8bd754adbf9f93bbb6b
Renil <Renil@GESS.LOCAL>
2026-02-05 18:18:06 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'features/auth/presentation/pages/login_page.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
 
-  SystemChrome.setEnabledSystemUIMode(
-    SystemUiMode.edgeToEdge,
-  );
-  
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.light,
-      systemNavigationBarDividerColor: Colors.transparent,
-    ),
-  );
-  
+  try {
+    await Firebase.initializeApp();
+    debugPrint('🔥 Firebase initialized successfully');
+  } catch (e, s) {
+    debugPrint('❌ Firebase initialization failed');
+    debugPrint(e.toString());
+    debugPrint(s.toString());
+  }
+
   runApp(const MyApp());
 }
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -35,19 +23,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mobile Ordering Application',
       debugShowCheckedModeBanner: false,
+      title: 'Concession Tracker',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF172B4D)),
-          useMaterial3: true,
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(fontFamily: 'DM Sans'),
-            bodyMedium: TextStyle(fontFamily: 'DM Sans'),
-            displayLarge: TextStyle(fontFamily: 'DM Sans', fontWeight: FontWeight.bold),
-            displayMedium: TextStyle(fontFamily: 'DM Sans', fontStyle: FontStyle.italic),
-            displaySmall: TextStyle(fontFamily: 'DM Sans', fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
-          ),
-        ),
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: false,
+      ),
       home: const LoginPage(),
     );
   }
